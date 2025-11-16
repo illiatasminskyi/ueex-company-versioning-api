594a9506c851a36097ca03a52d6d4770655d2597
illia.tasminskyi <illia.tasminskyi@gmail.com>
2025-11-16 15:12:16 +0200

    docker

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-// Redirect to API documentation
-Route::get('/docs', function () {
     return redirect('/api/documentation');
 });
